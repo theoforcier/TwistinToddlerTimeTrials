6b0a2f6b4a6ea0e4483975a097b06d03fb119b82
AryaM2001 <aryamattu@gmail.com>
2023-03-30 13:05:41 -0400

    minor changes from previous push

--- Assets/Scripts/Objects/BallonMovement.cs
+++ Assets/Scripts/Objects/BallonMovement.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class BallonMovement : MonoBehaviour
 {
     public float movementSpeed;
+    public GameObject baby;
     Rigidbody2D ridge;
+    
     Transform player;
+    Transform ballon;
     Vector2 direction;
-    bool check = true;
     float oldPos = 0.0f; 
+    private SpriteRenderer render;
+
 
     void Awake()
     {
         ridge = GetComponent<Rigidbody2D>();
+        ballon = GetComponent<Transform>();
     }
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").transform;
-        oldPos = transform.position.y;
+        render = GetComponent<SpriteRenderer>();
+       
     }
 
     // Update is called once per frame
@@ -28,21 +34,19 @@ public class BallonMovement : MonoBehaviour
         if(player)
         {
             Vector3 playerDirection = (player.position - transform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            ridge.rotation = angle;
-            direction = playerDirection;
-
-            transform.right = -direction;    
-            if(transform.position.y < oldPos)
-            {
-                transform.Rotate(new Vector3(180.0f, 0.0f, 0.0f));
-               
-            }
-
+           // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+           // ridge.rotation = angle;
+            direction = playerDirection;     
         }
+
+        if(baby.transform.position.y == ballon.position.y)
+        {
+            Debug.Log("Over");
+            FlipSprite();
+        }
+
+
      
-
-
     }
 
     void FixedUpdate()
@@ -52,5 +56,13 @@ public class BallonMovement : MonoBehaviour
             
             ridge.velocity = new Vector2(direction.x, direction.y) * movementSpeed;
         }
+
+
+    }
+
+    public void FlipSprite()
+    {
+        render.flipX = !render.flipX;
+        
     }
 }
--- Assets/Scripts/Objects/RobotMovement.cs
+++ Assets/Scripts/Objects/RobotMovement.cs
@@ -7,12 +7,16 @@ public class RobotMovement : MonoBehaviour
     public float movementSpeed;
     public int start;
     public Transform[] movementPoints;
+    private SpriteRenderer render;
+
     int i;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = movementPoints[start].position;    
+        transform.position = movementPoints[start].position;  
+        render = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,6 +25,7 @@ public class RobotMovement : MonoBehaviour
         if(Vector2.Distance(transform.position, movementPoints[i].position) < 0.02f)
         {
             i++;
+            FlipSprite();
             if (i == movementPoints.Length)
             {
                 i = 0; 
@@ -28,5 +33,11 @@ public class RobotMovement : MonoBehaviour
         }
 
         transform.position = Vector2.MoveTowards(transform.position, movementPoints[i].position, movementSpeed *Time.deltaTime);
+    }
+
+    public void FlipSprite()
+    {
+        render.flipX = !render.flipX;
+        
     }
 }
